12769a4986e66d942a53011cac1050fa9e98709e
Vaulo <150976164+Vaulo@users.noreply.github.com>
2023-11-16 19:44:26 -0300

    main.go

--- main.go
+++ main.go
@@ -1,11 +1,13 @@
+package main
+
 func main() {
-	chaincode, err := contractapi.NewChaincode(&MyContract{})
+	Chaincode-1, err := contractapi.NewChaincode(&MyContract{})
 	if err != nil {
 		fmt.Printf("Erro ao criar o chaincode: %s", err)
 		return
 	}
 
-	if err := chaincode.Start(); err != nil {
+	if err := Chaincode-1.Start(); err != nil {
 		fmt.Printf("Erro ao iniciar o chaincode: %s", err)
 	}
 }
