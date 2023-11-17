4c0e051a25fe9566892421f4d2e9ea01ec0e26b3
Vaulo <150976164+Vaulo@users.noreply.github.com>
2023-11-16 22:48:16 -0300

    Chaincodemove.go

--- Chaincodemove.go
+++ Chaincodemove.go
@@ -209,3 +209,14 @@ func calcularHash(data []byte) string {
 	return hex.EncodeToString(hasher.Sum(nil))
 }
 
+func main() {
+	Chaincodemove, err := contractapi.NewChaincode(&MyContract{})
+	if err != nil {
+		fmt.Printf("Erro ao criar o chaincode: %s", err)
+		return
+	}
+
+	if err := Chaincodemove.Start(); err != nil {
+		fmt.Printf("Erro ao iniciar o chaincode: %s", err)
+	}
+}
